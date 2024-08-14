045cb6001a14854b8f4ba032faabe82eb2e85fde
Arbiya Sultana <alra8006@gmail.com>
2024-08-14 07:16:15 +0530

    first commit

---
+++ src/Components/Main.js
@@ -0,0 +1,30 @@
+import react from 'react';
+import "./Main.css";
+
+
+function Main(){
+ 
+    return(
+        <div className="main">
+        <div className="Home">
+        
+            <div className='block'>
+            <div  className="header">
+        <div className='blue'>        
+            </div>
+            </div>
+            <div className='blacky'>
+            </div>
+            </div>
+        </div>
+        <div className="About"></div>
+        <div className="Service"></div>
+        <div className="Reviews"></div>
+        <div className="Contact"></div>
+
+
+    </div>
+        
+    );
+}
+export default Main;
