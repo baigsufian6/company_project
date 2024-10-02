44db53fb1d8d34c462f29fbacf7e06eed44c3563
baigsufian6 <baigsufian6@gmail.com>
2024-10-02 14:53:33 +0530

    construction project update

--- src/Components/Construction/ProjectConstruct.js
+++ src/Components/Construction/ProjectConstruct.js
@@ -1,33 +1,37 @@
-import {React,useEffect} from 'react';
+import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './construct.css';
 import Slider from 'react-slick';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import { ChevronLeft, ChevronRight } from 'lucide-react';
-import prop1 from './imagess/prop1.webp';
-import prop2 from './imagess/prop2.webp';
-import prop3 from './imagess/prop3.webp';
-import image1 from './imagess/image1.jpg';
-import image2 from './imagess/image2.jpg';
-import image3 from './imagess/image3.jpg';
+import prop1 from './imagess/3.jpg';
+import prop2 from './imagess/6.jpg';
+import prop3 from './imagess/1.jpg';
+import image1 from './imagess/7.jpg';
+import image2 from './imagess/8.jpg';
+import image3 from './imagess/5.jpg';
+import img2 from './imagess/2.jpg';
+import img4 from './imagess/4.jpg'
+import img9 from './imagess/9.jpg'
+import img10 from './imagess/10.jpg'
+import img11 from './imagess/11.jpg'
+import img12 from './imagess/12.jpg'
 
 // Custom arrow components
 const NextArrow = ({ onClick }) => (
-  <button className="slick-arrow slick-next" onClick={onClick}>
+  <div className="slick-arrow slick-next" onClick={onClick}>
     <ChevronRight size={24} />
-  </button>
+  </div>
 );
 
 const PrevArrow = ({ onClick }) => (
-  <button className="slick-arrow slick-prev" onClick={onClick}>
+  <div className="slick-arrow slick-prev" onClick={onClick}>
     <ChevronLeft size={24} />
-  </button>
+  </div>
 );
 
-
 function ProjectConstruct() {
-
   useEffect(() => {
     // Scroll to the top of the page
     window.scrollTo(0, 0);
@@ -40,7 +44,7 @@ function ProjectConstruct() {
 
   const settings = {
     dots: true,
-    infinite: false,
+    infinite: true,
     speed: 500,
     slidesToShow: 3,
     slidesToScroll: 1,
@@ -65,7 +69,6 @@ function ProjectConstruct() {
       }
     ]
   };
-  
 
   return (
     <div>
@@ -79,34 +82,60 @@ function ProjectConstruct() {
       </div>
 
       <section className="signature-designs">
-        <h2>Signature Designs</h2>
-        <h3>Lifting Home Aesthetics to New Heights</h3>
-        <Slider {...settings}>
-          <div className="design-card">
-            <img src={image1} alt="Project Ramesh Residency" />
-          </div>
+  <h2>Signature Designs</h2>
+  <h3>Lifting Home Aesthetics to New Heights</h3>
 
-          <div className="design-card">
-            <img src={image2} alt="Project Dharmesh Residency" />
-          </div>
+  {/* External View of Homes */}
+  <div className="slider-container">
+    <h4>External Views</h4>
+    <Slider {...settings}>
+      <div className="design-card">
+        <img src={image1} alt="Project Ramesh Residency External View" />
+      </div>
+      <div className="design-card">
+        <img src={image2} alt="Project Dharmesh Residency External View" />
+      </div>
+      <div className="design-card">
+        <img src={image3} alt="Project Sridhar Residency External View" />
+      </div>
+      <div className="design-card">
+        <img src={img4} alt="Project Internal View" />
+      </div>
+      <div className="design-card">
+        <img src={prop3} alt="Project Prop3 Internal View" />
+      </div>
+    </Slider>
+  </div>
 
-          <div className="design-card">
-            <img src={image3} alt="Project Sridhar Residency" />
-          </div>
+  {/* Internal View of Homes */}
+  <div className="slider-container">
+    <h4>Internal Views</h4>
+    <Slider {...settings}>
+      <div className="design-card">
+        <img src={prop1} alt="Project Prop1 Internal View" />
+      </div>
+      <div className="design-card">
+        <img src={prop2} alt="Project Prop2 Internal View" />
+      </div>
+      <div className="design-card">
+        <img src={img2} alt="Project Internal View" />
+      </div>
+      <div className="design-card">
+        <img src={img9} alt="Project Internal View" />
+      </div>
+      <div className="design-card">
+        <img src={img10} alt="Project Internal View" />
+      </div>
+      <div className="design-card">
+        <img src={img11} alt="Project Internal View" />
+      </div>
+      <div className="design-card">
+        <img src={img12} alt="Project Internal View" />
+      </div>
+    </Slider>
+  </div>
+</section>
 
-          <div className="design-card">
-            <img src={prop1} alt="Project Prop1" />
-          </div>
-
-          <div className="design-card">
-            <img src={prop2} alt="Project Prop2" />
-          </div>
-
-          <div className="design-card">
-            <img src={prop3} alt="Project Prop3" />
-          </div>
-        </Slider>
-      </section>
     </div>
   );
 }
